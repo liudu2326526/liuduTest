831ba00fc98d7cb315c99a938cb1506ae0fb236e
liudu <liudu@dayuwuxian.com>
2022-01-07 09:32:03 +0800

    Changes

--- aws/src/main/java/liudu/com/DynamodbTest.java
+++ aws/src/main/java/liudu/com/DynamodbTest.java
@@ -1,5 +1,12 @@
 package liudu.com;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputDescription;
+
 /**
  * @author liudu
  * @title: DynamodbTest
@@ -11,6 +18,21 @@ public class DynamodbTest {
 
   public static void main(String[] args) {
 
+    long writeCapacity = 1000l;
+
+    AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1)
+        .withCredentials(new DefaultAWSCredentialsProviderChain())
+        .build();
+
+    ProvisionedThroughputDescription adsProfileStaging = dynamoDB.describeTable("AdsProfileStaging")
+        .getTable().getProvisionedThroughput();
+
+//    System.out.println(adsProfileStaging.getReadCapacityUnits());
+    System.out.println(adsProfileStaging.getWriteCapacityUnits());
+
+    adsProfileStaging.setWriteCapacityUnits(writeCapacity);
+
+    System.out.println(adsProfileStaging.getWriteCapacityUnits());
   }
 
 }
