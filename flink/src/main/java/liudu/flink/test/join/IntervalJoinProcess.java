06a69c07f72a8166529e292a56f2352ee2fe83d9
username <email>
2022-06-22 16:13:14 +0800

    add export.sh

--- flink/src/main/java/liudu/flink/test/join/IntervalJoinProcess.java
+++ flink/src/main/java/liudu/flink/test/join/IntervalJoinProcess.java
@@ -2,9 +2,12 @@ package liudu.flink.test.join;
 
 import liudu.flink.test.bean.MyBeanData;
 import liudu.flink.test.source.MySource1;
+import liudu.flink.test.source.MySource2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
 /**
  * @author liudu
@@ -19,11 +22,18 @@ public class IntervalJoinProcess {
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     env.setParallelism(1);
     DataStreamSource<MyBeanData> ds1 = env.addSource(new MySource1());
-    DataStreamSource<MyBeanData> ds2 = env.addSource(new MySource1());
+    DataStreamSource<MyBeanData> ds2 = env.addSource(new MySource2());
 
-    ds1.keyBy(user->user.getId())
-        .intervalJoin(ds2.keyBy(user->user.getId()))
-        .between(Time.milliseconds(-5), Time.milliseconds(5));
+    ds1.keyBy(user -> user.getId())
+        .intervalJoin(ds2.keyBy(user -> user.getId()))
+        .between(Time.milliseconds(-5), Time.milliseconds(5))
+        .process(new ProcessJoinFunction<MyBeanData, MyBeanData, MyBeanData>() {
+          @Override
+          public void processElement(MyBeanData myBeanData, MyBeanData myBeanData2, Context context,
+              Collector<MyBeanData> collector) throws Exception {
+
+          }
+        });
 
   }
 
---
+++ flink/src/main/java/liudu/flink/test/source/MySource2.java
@@ -0,0 +1,42 @@
+package liudu.flink.test.source;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import liudu.flink.test.bean.MyBeanData;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+public class MySource2 implements SourceFunction<MyBeanData> {
+
+
+  private boolean running = true;
+
+  @Override
+  public void run(SourceContext<MyBeanData> ctx) throws Exception {
+
+    Random random = new Random();
+    Map<Integer, Double> tempMap = new HashMap<>();
+    for (int i = 0; i < 10; i++) {
+      tempMap.put(i + 1, 60 + random.nextGaussian() * 20);
+    }
+    while (true) {
+      for (Integer key : tempMap.keySet()) {
+        Double newTmp = tempMap.get(key) + random.nextGaussian();
+        tempMap.put(key, newTmp);
+//        MyBeanData myBeanData = new MyBeanData(key, System.currentTimeMillis(), "liudu" + key);
+//        ctx.collectWithTimestamp(myBeanData, System.currentTimeMillis());
+        ctx.collect(new MyBeanData(key, System.currentTimeMillis() + random.nextInt(10) * 1000,
+            "niupi" + key / 2));
+        Thread.sleep(1000L);
+      }
+
+//      System.out.println("======================");
+    }
+
+  }
+
+  @Override
+  public void cancel() {
+    this.running = false;
+  }
+}
