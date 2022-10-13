52d9245ffd09de9893736244232bdeeb61cc77eb
liudu <liudu2326526>
2022-10-13 10:42:18 +0800

    11

---
+++ huawei/src/main/resources/create_ddl/ods.sql
@@ -0,0 +1,55 @@
+-- prod_ods.ods_mip_author_raw
+
+DROP TABLE prod_ods.ods_mip_author_raw;
+
+CREATE TABLE prod_ods.ods_mip_author_raw
+(
+    data STRING
+)
+    PARTITIONED BY (dt STRING,hour STRING)
+    STORED AS PARQUET
+    LOCATION 'obs://donson-mip-data/prod/ods/author';
+
+MSCK REPAIR TABLE prod_ods.ods_mip_author_raw;
+
+-- prod_ods.ods_mip_comment_raw
+
+DROP TABLE prod_ods.ods_mip_comment_raw;
+
+CREATE TABLE prod_ods.ods_mip_comment_raw
+(
+    data STRING
+)
+    PARTITIONED BY (dt STRING,hour STRING)
+    STORED AS PARQUET
+    LOCATION 'obs://donson-mip-data/prod/ods/comment';
+
+MSCK REPAIR TABLE prod_ods.ods_mip_comment_raw;
+
+-- prod_ods.ods_mip_article_raw
+
+DROP TABLE prod_ods.ods_mip_article_raw;
+
+CREATE TABLE prod_ods.ods_mip_article_raw
+(
+    data STRING
+)
+    PARTITIONED BY (dt STRING,hour STRING)
+    STORED AS PARQUET
+    LOCATION 'obs://donson-mip-data/prod/ods/article';
+
+MSCK REPAIR TABLE prod_ods.ods_mip_article_raw;
+
+-- prod_ods.ods_mip_media_raw
+
+DROP TABLE prod_ods.ods_mip_media_raw;
+
+CREATE TABLE prod_ods.ods_mip_media_raw
+(
+    data STRING
+)
+    PARTITIONED BY (dt STRING,hour STRING)
+    STORED AS PARQUET
+    LOCATION 'obs://donson-mip-data/prod/ods/media';
+
+MSCK REPAIR TABLE prod_ods.ods_mip_media_raw;
