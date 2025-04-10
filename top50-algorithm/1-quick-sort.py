13f051542e622699773e837a43cb6b7a65b4deab
liudu <liudu2326526>
2025-04-10 09:45:04 +0800

    11

--- top50-algorithm/1-quick-sort.py
+++ top50-algorithm/1-quick-sort.py
@@ -1,20 +1,38 @@
-def quick_sort(arr):
-  # 递归终止条件：当列表长度小于等于1时，直接返回
-  if len(arr) <= 1:
-    return arr
+def quick_sort(arr, left, right):
+  # 终止条件
+  if right > left:
+    pivot_index = partition(arr, left, right)
+    quick_sort(arr, left, pivot_index - 1)
+    quick_sort(arr, pivot_index + 1, right)
+  return arr
 
-  # 选择基准值（这里选择第一个元素）
-  pivot = arr[0]
 
-  # 分别存放小于、等于和大于 pivot 的元素
-  left = [x for x in arr[1:] if x < pivot]
-  middle = [x for x in arr if x == pivot]
-  right = [x for x in arr[1:] if x > pivot]
+def partition(arr, low, high):
+  """
+  分区函数，将数组分为两部分，使得 pivot 左边都小于 pivot，右边都大于 pivot。
+  :param arr: 待排序的列表
+  :param low: 分区起始索引
+  :param high: 分区结束索引（pivot 选择此位置的值）
+  :return: pivot 排序后的正确索引位置
+  """
+  # 选择最后一个数作为基准
+  pivot = arr[high]
+  i = low - 1
+  for j in range(low, high):
+    if pivot < arr[j]:
+      i += 1
+      # 交换位置
+      arr[i], arr[j] = arr[j], arr[i]
+  # 最后把 pivot 放到它的正确位置（i+1）
 
-  # 递归排序并拼接结果
-  return quick_sort(left) + middle + quick_sort(right)
+  arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+  return i + 1
+
 
 # 示例
-arr = [3, 6, 8, 10, 1, 2, 1]
-sorted_arr = quick_sort(arr)
-print("排序结果：", sorted_arr)
+arr = [6, 3, 1, 10, 1, 2, 8]
+sorted_arr = quick_sort(arr, 0, len(arr) - 1)
+# sorted_arr = quick_sort(arr)
+print("排序结果：", arr)
+print(sorted_arr)
